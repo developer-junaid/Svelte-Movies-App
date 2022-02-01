e8ba9913b53dd44f2a75466732e62284640c9f97
Developer Junaid <qjunaid623@gmail.com>
2022-02-01 12:43:45 +0500

    added next adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,9 @@
-import adapter from '@sveltejs/adapter-netlify';
+import netlify from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app
-			split: false
-		}),
+		adapter: netlify(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
